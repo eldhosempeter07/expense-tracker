71a4bfdd0eda667a2a67afffdde66f0a313b9bde
eldhosempeter07 <eldhosempeter07@gmail.com>
2024-06-10 16:32:10 -0400

    fix: Update project structure

--- client/src/utils/utils.ts
+++
@@ -1,9 +0,0 @@
-export const formatDate = (dateString: string) => {
-  const date = new Date(dateString);
-  // Get the year, month, and day components
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, "0");
-  const day = String(date.getDate()).padStart(2, "0");
-  // Return the formatted date string
-  return `${year}-${month}-${day}`;
-};
